ded0a43c55fa1f29161c2893615571d21fae494b
Mmasetshaba Rakgalakane <rakgalakanemaschiie@gmail.com>
2026-07-22 18:12:18 +0200

    Add project files.

---
+++ CHABA.DayCare/Models/Core/Classroom.cs
@@ -0,0 +1,33 @@
+﻿using CHABA.DayCare.Models.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace CHABA.DayCare.Models.Core
+{
+    public class Classroom : BaseEntity
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [StringLength(250)]
+        public string? Description { get; set; }
+
+        [Required]
+        public int MinimumAgeInMonths { get; set; }
+
+        public int? MaximumAgeInMonths { get; set; }
+
+        [Required]
+        [Range(1, 100)]
+        public int Capacity { get; set; }
+
+
+        //Foreign key to Organisation
+        public int OrganisationId { get; set; }
+
+        //Navigation property to Organisation
+        public Organisation Organisation { get; set; } = null!;
+
+        //public ICollection<Child> Children { get; set; } = new List<Child>();
+    }
+}
---
+++ CHABA.DayCare/Models/Core/Organisation.cs
@@ -0,0 +1,29 @@
+﻿using CHABA.DayCare.Models.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace CHABA.DayCare.Models.Core
+{
+    public class Organisation : BaseEntity
+    {
+
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        public string? RegistrationNumber { get; set; }
+
+        [StringLength(20)]
+        public string? PhoneNumber { get; set; }
+
+        [EmailAddress]
+        public string? Email {  get; set; }
+
+        [StringLength(250)]
+        public string? PhysicalAddress { get; set; }
+
+        public string? LogoUrl { get; set; }
+
+        public bool IsActive { get; set; } = true;
+    }
+}
